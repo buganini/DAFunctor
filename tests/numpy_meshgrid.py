f67daac1d749860e57f861916d83a0a2aaee1507
Buganini Chiu <buganini@b612.tw>
2021-03-07 14:28:53 +0800

    add slices meshgrid tests

--- tests/numpy_meshgrid.py
+++ tests/numpy_meshgrid.py
@@ -10,3 +10,13 @@ def f2(np):
     return np.array(np.meshgrid(np.arange(1,3),np.arange(3,6),[7,8,9,10]))
 
 test_func("meshgrid_arange", f2)
+
+def f3(np):
+    return np.meshgrid(np.arange(1,3),np.arange(3,6),[7,8,9,10])[1]
+
+test_func("meshgrid_slice1", f3)
+
+def f4(np):
+    return np.meshgrid(np.arange(1,3),np.arange(3,6),[7,8,9,10])[2]
+
+test_func("meshgrid_slice2", f4)
