fix off-by-one length error in numpy.arange()

tests/numpy_arange.py
@@ -1,6 +1,11 @@
 from tester_numpy import *
 
-def f(np):
+def f1(np):
     return np.arange(1,5,2)
 
-test_func("arange", f)
+test_func("arange", f1)
+
+def f2(np):
+    return np.arange(1,5000,7)
+
+test_func("arange_long", f2)