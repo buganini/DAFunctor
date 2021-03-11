5733cc750525712cdca7ce728785a524807a1aea
Buganini Q <buganini@b612.tw>
2021-03-11 10:05:45 +0800

    update tests

--- tests/numpy_math.py
+++ tests/numpy_math.py
@@ -14,3 +14,13 @@ def add3(np):
     return np.array([[4,5],[6,7]]) + 2 + 3
 
 test_func("add3", add3)
+
+def stack_add(np):
+    return np.stack([np.array([[1,2,3],[4,5,6]]), np.array([[7,8,9],[10,11,12]])], axis=0) + 1
+
+test_func("stack_add", stack_add)
+
+def stack_transpose_add(np):
+    return np.transpose(np.stack([np.array([[1,2],[4,5]]), np.array([[7,8],[10,11]])], axis=0), (1,0,2)) + 1
+
+test_func("stack_transpose_add", stack_transpose_add)
--- tests/numpy_mixed.py
+++
@@ -1,6 +0,0 @@
-from tester_numpy import *
-
-def f1(np):
-    return np.stack([np.array([[1,2,3],[4,5,6]]), np.array([[7,8,9],[10,11,12]])], axis=0) + 1
-
-test_func("mixed1", f1)
