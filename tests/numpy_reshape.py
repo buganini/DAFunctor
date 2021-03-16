03786ebfb9189c1cd5010ae211bd383a80880732
Buganini Q <buganini@b612.tw>
2021-03-17 04:17:20 +0800

    more tests

--- tests/numpy_reshape.py
+++ tests/numpy_reshape.py
@@ -11,3 +11,9 @@ def f2(np):
     return np.reshape(np.reshape(np.array(a), (1,2,1,3,1)), (3,2))
 
 test_func("reshape2", f2)
+
+def f3(np):
+    a = [[1,2,3,4]]
+    return np.reshape(np.array(a), (2,2))
+
+test_func("reshape3", f3)
