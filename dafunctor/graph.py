5d4142205c013ebf65254340caf27530d8ec075e
Buganini Q <buganini@b612.tw>
2021-05-09 08:56:37 +0800

    visualizer: draw opdesc as arrow label

--- dafunctor/graph.py
+++ dafunctor/graph.py
@@ -2,8 +2,9 @@ import subprocess
 
 def draw_graph(functor):
     def walk(f, functor):
+        f.write(f"\t{functor.get_name()}\n")
         for s in functor.subs:
-            f.write(f"\t{s.get_name()} -> {functor.get_name()};\n")
+            f.write(f"\t{s.get_name()} -> {functor.get_name()} [label=\"{functor.opdesc}\"];\n")
             walk(f, s)
 
     tmp = functor.get_tmp()
